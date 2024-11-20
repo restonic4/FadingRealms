967f36041380b3a3216a99c098d374f0155eee42
restonic4 <marcosruizlerma@gmail.com>
2024-11-20 20:53:11 +0100

    Update

--- common/src/main/java/me/restonic4/fading_realms/gui/IntroWaitScreen.java
+++ common/src/main/java/me/restonic4/fading_realms/gui/IntroWaitScreen.java
@@ -3,6 +3,7 @@ package me.restonic4.fading_realms.gui;
 import me.restonic4.fading_realms.util.Camera.Cutscene.Easing;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.ImageWidget;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
@@ -20,27 +21,23 @@ public class IntroWaitScreen extends Screen {
     public ImageWidget title;
     public ImageWidget waitingText;
 
-    /*public int titleWidth = (int)(2047f/3f);
-    public int titleHeight = (int)(868f/3f);*/
-    public int titleWidth = 256;
-    public int titleHeight = 44;
-
-    public int title_X = (int)((width / 2f) - (titleWidth / 2f));
-    public int title_Y = titleHeight;
-
-    /*public int waitingTextWidth = (int)(2005f/3f);
-    public int waitingTextHeight = (int)(282f/3f);*/
-    public int waitingTextWidth = 334;
-    public int waitingTextHeight = 47;
-
-    public int waitingText_X = (int)((width / 2f) - (waitingTextWidth / 2f));
-    public int waitingText_Y = height - waitingTextHeight;
-
     @Override
     protected void init() {
+        int title_w = (int)((1024f / 2) / 1.5f);
+        int title_h = (int)((256f / 2) / 1.5f);
+
+        int title_x = (int)((width / 2f) - (title_w / 2));
+        int title_y = (int)(0f + (title_h / 2));
+
+        int waitingText_w = (int)((1024f / 2) / 2.5f);
+        int waitingText_h = (int)((256f / 2) / 2.5f);
+
+        int waitingText_x = (int)((width / 2f) - (waitingText_w / 2));
+        int waitingText_y = (int)(height - waitingText_h - (waitingText_h / 2f));
+
         background = new ImageWidget(0, 0, width, height, new ResourceLocation("fading_realms", "textures/gui/WaitIntroBackground.png"));
-        title = new ImageWidget(title_X, title_Y, titleWidth, titleHeight, new ResourceLocation("fading_realms", "textures/gui/title.png"));
-        waitingText = new ImageWidget(waitingText_X, waitingText_Y, waitingTextWidth, waitingTextHeight, new ResourceLocation("fading_realms", "textures/gui/ElegidoText.png"));
+        title = new ImageWidget(title_x, title_y, title_w, title_h, new ResourceLocation("fading_realms", "textures/gui/title.png"));
+        waitingText = new ImageWidget(waitingText_x, waitingText_y, waitingText_w, waitingText_h, new ResourceLocation("fading_realms", "textures/gui/waitingText.png"));
 
         addRenderableWidget(background);
         addRenderableWidget(title);
